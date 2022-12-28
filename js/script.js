62c82ee7a24fcff95138478e816066e8845d32cb
seochaehee9177 <120554092+seochaehee9177@users.noreply.github.com>
2022-12-28 10:45:55 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,56 @@
-AOS.init({
-  duration: 1200
-})
+ $(function(){
+ var swiper = new Swiper('.one', {
+	  autoplay: {
+        delay: 5000,//슬라이드 넘어가는 시간
+        disableOnInteraction: false,
+      },
+      loop: true,//슬라이드 무한반복
+      navigation: {//화살표 버튼
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	    pagination: {//블릿 버튼
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+});
+
+
+ $(function(){
+ var swiper = new Swiper('.two', {
+	  autoplay: {
+        delay: 5000,//슬라이드 넘어가는 시간
+        disableOnInteraction: false,
+      },
+      loop: true,//슬라이드 무한반복
+      navigation: {//화살표 버튼
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	    pagination: {//블릿 버튼
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+});
+
+$(function(){
+  var swiper = new Swiper('.three', {
+     autoplay: {
+         delay: 5000,//슬라이드 넘어가는 시간
+         disableOnInteraction: false,
+       },
+       loop: true,//슬라이드 무한반복
+       navigation: {//화살표 버튼
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev',
+       },
+       pagination: {//블릿 버튼
+         el: '.swiper-pagination',
+         clickable: true,
+       },
+     });
+ });
+ 
+ 
